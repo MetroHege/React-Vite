1a70be571b9a83690bdb3f5a5f31842bd52e6b32
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-02-09 10:10:00 +0200

    contexts done

--- src/views/Logout.tsx
+++ src/views/Logout.tsx
@@ -1,4 +1,13 @@
+import {useEffect} from 'react';
+import {useUserContext} from '../hooks/ContextHooks';
+
 const Logout = () => {
+  const {handleLogout} = useUserContext();
+
+  useEffect(() => {
+    handleLogout();
+  }, []);
+
   return <p>log out!</p>;
 };
 
